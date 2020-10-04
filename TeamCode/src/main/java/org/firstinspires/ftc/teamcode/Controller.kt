a0fe225f443def764f7a3fabe68d42488dee1f42
Vap0r1ze <superdash993@gmail.com>
2020-10-04 01:25:20 -0400

    kotlin refactor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller.kt
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.util.ElapsedTime
+
+@TeleOp(name = "Manual Driving", group = "Driving")
+class Controller : LinearOpMode() {
+    private val runtime = ElapsedTime()
+    override fun runOpMode() {
+        telemetry.addData("Status", "Initializing...")
+        telemetry.update()
+        val dt = DriveTrain(hardwareMap)
+        dt.halfDtLength = 6.0
+        dt.halfDtWidth = 6.0
+        dt.wheelR = 2.0
+        telemetry.addData("Status", "Initialized")
+        telemetry.update()
+        waitForStart()
+        runtime.reset()
+        while (opModeIsActive()) {
+            dt.vx = gamepad1.left_stick_x.toDouble()
+            dt.vy = -gamepad1.left_stick_y.toDouble()
+            dt.w0 = gamepad1.right_stick_x.toDouble()
+            dt.doLogic()
+            dt.handleHardware()
+            telemetry.addData("<Vx, Vy, W0>", "<%.3f, %.3f, %.3f>", dt.vx, dt.vy, dt.w0)
+            telemetry.addData("<W1, W2, W3, W4>", "<%.3f, %.3f, %.3f, %.3f>", dt.wn[0], dt.wn[1], dt.wn[2], dt.wn[3])
+            telemetry.update()
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.kt
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.Servo
+import kotlin.math.abs
+
+internal class DriveTrain(hardwareMap: HardwareMap) {
+    private val dtMotors: Array<DcMotor>
+    var wheelR = 0.0
+    var halfDtWidth = 0.0
+    var halfDtLength = 0.0
+    var vx = 0.0
+    var vy = 0.0
+    var w0 = 0.0
+    val wn: DoubleArray
+    private val motorGroups: Array<Array<DcMotor>>
+    private val servoGroups: Array<Array<Servo>>
+
+    /* <w1, w2, w3, w4> = 1/r [<1, 1, 1, 1> <1, -1, -1, 1> (L1 + L2)<-1, 1, -1, 1>] <vx,vy, w0> */
+    fun doLogic() {
+        var max = 0.0
+        for (i in 0..3) {
+            var w = 0.0
+            w += vx
+            w += (if (i % 3 == 0) -1 else 1) * vy
+            w += (if (i % 2 == 0) -1 else 1) * (halfDtLength + halfDtWidth) * w0
+            wn[i] = w
+            if (abs(w) > max) {
+                max = abs(w)
+            }
+        }
+        if (max > 0) {
+            for (i in 0..3) {
+                wn[i] /= max
+            }
+        }
+    }
+
+    fun handleHardware() {
+        for (i in 0..3) {
+            dtMotors[i].power = wn[i]
+        }
+    }
+
+    init {
+        val dtLeftFront = hardwareMap.get(DcMotor::class.java, "dt lf")
+        val dtRightFront = hardwareMap.get(DcMotor::class.java, "dt rf")
+        val dtLeftBack = hardwareMap.get(DcMotor::class.java, "dt lb")
+        val dtRightBack = hardwareMap.get(DcMotor::class.java, "dt rb")
+        dtMotors = arrayOf(dtLeftFront, dtRightFront, dtLeftBack, dtRightBack)
+        motorGroups = arrayOf(dtMotors)
+        servoGroups = arrayOf()
+        for (motorGroup in motorGroups) {
+            for (i in motorGroup.indices) {
+                motorGroup[i].mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+                motorGroup[i].direction = if (i % 2 == 0) DcMotorSimple.Direction.FORWARD else DcMotorSimple.Direction.REVERSE
+                motorGroup[i].zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+            }
+        }
+        for (servoGroup in servoGroups) {
+            for (i in servoGroup.indices) {
+                servoGroup[i].direction = if (i % 2 == 0) Servo.Direction.FORWARD else Servo.Direction.REVERSE
+            }
+        }
+        wn = doubleArrayOf(0.0, 0.0, 0.0, 0.0)
+    }
+}
